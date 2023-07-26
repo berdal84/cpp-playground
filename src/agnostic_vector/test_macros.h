aa6331b8250234b8c0d4d7bfc112e8fb7b7568a9
berdal84 <berenger@42borgata.com>
2023-07-26 00:48:18 -0400

    logs

--- src/agnostic_vector/test_macros.h
+++ src/agnostic_vector/test_macros.h
@@ -5,14 +5,15 @@ static size_t test_ok_count = 0;
 static size_t test_failed_count = 0;
 
 #define TEST(name, expression) \
-    printf("==== Test n°%03zu: %-25s =========[ ", test_count+1, name ); \
+    printf("==== Test n°%03zu: %-25s =========[ ", test_count+1, name); \
     if ( expression ) { \
         printf("%s", "OK ] :)\n"); \
         ++test_ok_count; \
     } else { \
-        printf("%s", "FAILED ] :(\n"); \
+        printf("%s", "FAILED ] :(\n\n"); \
         ++test_failed_count;            \
-        printf("\n expression: %s", #expression "\n\n"); \
+        printf("\texpression evaluated false: %s", #expression "\n");                        \
+        printf("\tlocation: %s (line %d)\n\n",  __FILE__, __LINE__ ); \
     }\
     ++test_count;
 
