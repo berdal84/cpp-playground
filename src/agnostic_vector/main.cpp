4ef05c0386a97a8bd8ac4c7dbf934f1e6f757261
berdal84 <berenger@42borgata.com>
2023-07-26 00:21:46 -0400

    fix comments, remove useless using

--- src/agnostic_vector/main.cpp
+++ src/agnostic_vector/main.cpp
@@ -3,15 +3,13 @@
 #include "component.h"
 
 int main() {
-    // static and dynamic parameters:
-    using TheComponentIWant = Component;
-    constexpr auto element_size = sizeof(TheComponentIWant);
+    constexpr auto element_size = sizeof(Component);
     {
         // Construct a vector
         size_t desired_initial_capacity = 2;
         agnostic_vector<element_size> vec{desired_initial_capacity};
-        auto default_constructor = [](void *ptr) -> void* { return new(ptr) TheComponentIWant(0); };
-        vec.init_for<TheComponentIWant>(default_constructor);
+        auto default_constructor = [](void *ptr) -> void* { return new(ptr) Component(0); };
+        vec.init_for<Component>(default_constructor);
 
         TEST("capacity", vec.buffer.capacity() == vec.capacity());
         TEST("capacity", vec.capacity() == desired_initial_capacity);
@@ -20,18 +18,18 @@ int main() {
 
         printf("\nemplace_back<Component>(int) component1 ...\n\n");
 
-        auto *component1 = vec.emplace_back<TheComponentIWant>(42); // uses internal default_constructor lambda
+        auto *component1 = vec.emplace_back<Component>(42);
         TEST("Component's value", component1->value == 42);
         TEST("Component's value", component1->deleted == false);
         TEST("size", vec.size() == 1);
 
-        printf("\nemplace_back() ...\n\n");
-        vec.emplace_back(); // uses internal default_constructor lambda
+        printf("\nemplace_back(), cast and assign value ...\n\n");
+        auto c = (Component*)vec.emplace_back(); // <----------- this uses internally the default_constructor lambda
+        c->value = 2023;
 
         printf("\nget_at<TheComponentIWant>() ...\n\n");
-        auto *component2 = vec.at<TheComponentIWant>(1);
-
-        TEST("Component's value", component2->value == 0);
+        auto component2 = vec.at<Component>(1);
+        TEST("Component's value", component2->value == 2023);
 
         TEST("size", vec.size() == 2);
 
@@ -44,10 +42,10 @@ int main() {
         vec.resize(4);
 
         TEST("size", vec.size() == 4);
-        TEST("buffer_size_in_bytes", vec.buffer_size_in_bytes() == sizeof(TheComponentIWant) * 4);
+        TEST("buffer_size_in_bytes", vec.buffer_size_in_bytes() == sizeof(Component) * 4);
         TEST("capacity", vec.capacity() >= 4);
     }
-    TEST("instance_count is zero", TheComponentIWant::instance_count() == 0);
+    TEST("instance_count is zero", Component::instance_count() == 0);
 
     TEST_RESULTS;
 }
